a2ab02e070dc813fb61786fb10f274dcfcd99160
JaheonKu <jaheon@wayties.com>
2024-02-29 17:41:07 +0900

    1

---
+++ Ex1/src/Project17.dpr
@@ -0,0 +1,14 @@
+program Project17;
+
+uses
+  Vcl.Forms,
+  Unit17 in 'Unit17.pas' {Form17};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm17, Form17);
+  Application.Run;
+end.
